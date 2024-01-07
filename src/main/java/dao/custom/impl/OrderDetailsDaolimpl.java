9f42d63c08da7f2c84ad4e9177b728d71f226b34
kavijarr <kavijakumuditha12@gmail.com>
2024-01-07 11:48:42 +0530

    search Item/Customer to hibernate

--- src/main/java/dao/custom/impl/OrderDetailsDaolimpl.java
+++ src/main/java/dao/custom/impl/OrderDetailsDaolimpl.java
@@ -13,18 +13,19 @@ import java.util.List;
 public class OrderDetailsDaolimpl implements OrderDetailsDao {
     @Override
     public boolean saveOrderDetails(List<OrderDetailsDto> list) throws SQLException, ClassNotFoundException {
-        boolean isDetailsSaved = true;
-        for (OrderDetailsDto dto:list) {
-            String sql = "INSERT INTO orderdetail VALUES(?,?,?,?)";
-            PreparedStatement pstm = DBConection.getInstance().getConnection().prepareStatement(sql);
-            pstm.setString(1,dto.getOrderID());
-            pstm.setString(2,dto.getItemCode());
-            pstm.setInt(3,dto.getQty());
-            pstm.setDouble(4,dto.getUnitPrice());
-            if (!(pstm.executeUpdate() >0)){
-                isDetailsSaved = false;
-            }
-        }
-        return isDetailsSaved;
+//        boolean isDetailsSaved = true;
+//        for (OrderDetailsDto dto:list) {
+//            String sql = "INSERT INTO orderdetail VALUES(?,?,?,?)";
+//            PreparedStatement pstm = DBConection.getInstance().getConnection().prepareStatement(sql);
+//            pstm.setString(1,dto.getOrderID());
+//            pstm.setString(2,dto.getItemCode());
+//            pstm.setInt(3,dto.getQty());
+//            pstm.setDouble(4,dto.getUnitPrice());
+//            if (!(pstm.executeUpdate() >0)){
+//                isDetailsSaved = false;
+//            }
+//        }
+//        return isDetailsSaved;
+        return true;
     }
 }
